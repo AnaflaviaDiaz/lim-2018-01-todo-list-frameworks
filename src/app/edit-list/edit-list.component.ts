133859cbfa69bd7be3f61b9dc7f83255326d5d06
anaflaviadiaz <anaflaviadiazmartel5a@gmail.com>
2018-10-01 18:18:22 -0500

    cancel edit

--- src/app/edit-list/edit-list.component.ts
+++ src/app/edit-list/edit-list.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditListComponent implements OnInit {
 
+  // texto: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(value): void {
+    console.log(value);
   }
 
 }
