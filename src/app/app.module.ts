3c4889affdb374c64ca796a315a6e04ef866e02b
anaflaviadiaz <anaflaviadiazmartel5a@gmail.com>
2018-10-01 17:35:48 -0500

    main interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EditListComponent } from './edit-list/edit-list.component';
+import { ShowListComponent } from './show-list/show-list.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditListComponent,
+    ShowListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
